697a1fbbcdbbdf055d141f26de4d03684c6b2e2d
andres lopez <zeroxandres@gmail.com>
2024-09-10 21:01:58 -0500

    backend report_petclub v0.2.5

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,19 @@ import "dotenv/config";
 import cors from "cors";
 import { dbConection2 } from "./config/dbcontection";
 import { router } from "./routes";
+import path from "path";
 
-const PORT = Number(process.env.PORT) || 3001;
-const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Middleware para servir archivos estáticos desde el directorio 'public'
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Ruta para servir el archivo HTML específico en la ruta '/'
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 app.use(cors({
     origin: '*',
@@ -16,4 +24,4 @@ app.use(cors({
 app.use(express.json());
 app.use(router);
 dbConection2().then(()=>console.log('BD2 Conectada al Server Reporteria😎'));
-app.listen(PORT, HOST, ()=> console.log(`Conectados desde el puerto ${PORT}`))
+app.listen(PORT, ()=> console.log(`Conectados desde el puerto ${PORT}`))
